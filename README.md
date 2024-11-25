f17d20f443e7c453f1ca6203b2a0daa0c37e9ca5
madhannkumarv <mkmadhan150@gmail.com>
2024-11-25 14:23:44 +0530

    Success

--- README.md
+++ README.md
@@ -151,7 +151,7 @@ Execute the program using runserver command.
 
 ## OUTPUT
 
-![alt text](<Screenshot (29).png>)
+![alt text](<Screenshot (30).png>)
 
 ## RESULT
 The program for creating slot timetable using basic HTML tags is executed successfully.
--- vmk/vmk/settings.py
+++ vmk/vmk/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=(os.path.join(BASE_DIR,'vmkapp/static'))
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'vmkapp/static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
